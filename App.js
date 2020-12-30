fb0f5e874afb7a44dc2ed3ee397220006c9ab2ba
anjaliahlawat16 <anjali@primesellerhub.com>
2020-12-30 08:42:30 +0530

    app initiated

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableHighlight} from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => {
+    console.log('clicked')
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Hello React Native. Welcome back</Text>
+      <TouchableHighlight>
+        <Image source={{
+          width: 200,
+          height: 200,
+          uri : "https://picsum.photos/200/300"}} />
+      </TouchableHighlight>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
