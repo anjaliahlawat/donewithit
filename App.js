98337780d8d714a50d97dca7bf4c392a5af1f786
anjaliahlawat16 <anjali@primesellerhub.com>
2021-01-31 11:50:34 +0530

    formik

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
-import React, {useState} from 'react';
-import { Switch, Text, TextInput, View } from 'react-native';
-import AppPicker from './app/components/AppPicker';
-import AppTextInput from './app/components/AppTextInput';
-import Screen from './app/components/Screen';
+import React from 'react';
+import { Text } from 'react-native';
 import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0])
 
   return (
     <LoginScreen />
   );
   
+
 }
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,40 +1,54 @@
-import React, { useState } from 'react';
-import { Image, Stylesheet } from 'react-native';
-import AppTextInput from '../components/AppTextInput';
-import AppButton from '../components/Button/Button';
+import React from 'react';
+import { Image, StyleSheet } from 'react-native';
+import * as Yup from 'yup';
+
+import {AppForm, AppFormField, SubmitButton} from '../components/forms';
 import Screen from '../components/Screen';
-import styles from '../config/styles';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label('Email'),
+  password: Yup.string().required().min(4).label('Password')
+})
 
 function LoginScreen(props) {
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
   return (
-    <Screen>
+    <Screen style={styles.container}>
         <Image 
           style={styles.logo}
           source={require('../assets/logo-red.png')} />
-        <AppTextInput 
-          placeholder="Email"
-          icon="email"
-          autoCapitalize="none"
-          autoCorrect={false}
-          onChangeText={text => setEmail(text)}
-          keyboardType="email-address"
-        />
-        <AppTextInput 
-          autoCapitalize="none"
-          autoCorrect={false}
-          onChangeText={text => setPassword(text)}
-          icon="lock"
-          secureTextEntry
-          placeholder="Password"
-        />
-        <AppButton title="Login" onPress={() => console.log()}/>
+
+          <AppForm
+            initialValues={{email: '', password:''}}
+            onSubmit={values=> console.log(values)}
+            validationSchema={validationSchema}>
+                <AppFormField 
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    icon="email"
+                    keyboardType="email-address"
+                    name="email"
+                    placeholder="Email"
+                />
+                    
+                <AppFormField 
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  icon="lock"
+                  name="password"
+                  placeholder="Password"
+                  secureTextEntry
+                />
+                <SubmitButton title="Login" />
+          </AppForm>
+         
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
+  container:{
+    padding: 10
+  },
   logo:{
     width: 80,
     height: 80,
